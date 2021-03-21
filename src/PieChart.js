87db5805b5da501ecb42b550c03335690887ba1d
bbenedict <bbenedict@gmail.com>
2021-03-21 11:33:16 -0700

    re-factor pie chart

--- src/PieChart.js
+++ src/PieChart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import * as d3 from "d3";
+import { group, pie, arc } from "d3";
 import data from './data/my_weather_data.json';
 
 export default function PieChart() {
@@ -16,31 +16,41 @@ export default function PieChart() {
     height: canvasSize.height - (2 * margin)
   };
 
-  const iconAccessor = d => d.icon;
-  const datasetGroup = d3.group(data, iconAccessor);
-  const datasetByIcon = [...datasetGroup]
+  const groupings = group(data, d => d.icon);
+  const totals = [...groupings]
     .map(([name, value]) => ({ name, value: value.length }))
     .sort((a,b) => b.value - a.value);
-  const otherCount = datasetByIcon.slice(4).reduce((acc, cur) => {
-    return acc + cur.value;
-  }, 0);
 
-  const combinedDatasetByIcon = [
-    ...datasetByIcon.slice(0, 4),
+  const pieData = [
+    ...totals.slice(0, 4),
     {
       name: "other",
-      value: otherCount
+      value: totals.slice(4).reduce((acc, cur) => { return acc + cur.value; }, 0)
     }
   ];
 
-  const arcGenerator = d3.pie()
-    .padAngle(0.005)
-    .value(d => d.value);
-
-  const arcs = arcGenerator(combinedDatasetByIcon);
+  const sliceGenerator = pie().padAngle(0.005).value(d => d.value);
+  const pieSlices = sliceGenerator(pieData);
 
   const radius = chartSize.width / 2;
-  const arc = d3.arc().innerRadius(radius * 0.7).outerRadius(radius);
+  const arcSVGData = arc().innerRadius(radius * 0.6).outerRadius(radius);
+
+  const arcLabel = (slice, centroid, index) => {
+    const x = centroid(slice)[0] - Math.ceil((slice.data.value.toString().length / 2) * 10);
+    const y = centroid(slice)[1] + 5;
+    return (
+      <text
+        key={`label${index}`}
+        transform={`translate(${x},${y})`}
+        fontSize="22px"
+        fill="white"
+      >
+        {slice.data.value}
+      </text>
+    );
+  };
+
+  const colors = ['#737CA1','#566D7E','#616D7E','#98AFC7','#837E7C'];
 
   return (
     <div>
@@ -48,19 +58,14 @@ export default function PieChart() {
       <svg width={canvasSize.width} height={canvasSize.height}>
         <g transform={`translate(${margin},0)`}>
           <g transform={`translate(${chartSize.height / 2}, ${chartSize.width / 2})`}>
-            {arcs.map((d, i) => (
-              <g key={i}>
+            {pieSlices.map((slice, index) => (
+              <g key={index}>
                 <path
-                  key={`arc${i}`}
-                  fill="#dadadd"
-                  d={arc(d)}
+                  key={`slice${index}`}
+                  fill={colors[index]}
+                  d={arcSVGData(slice)}
                 />
-                <text
-                  key={`label${i}`}
-                  transform={`translate(${arc.centroid(d)})`}
-                >
-                  {d.data.value}
-                </text>
+                { arcLabel(slice, arcSVGData.centroid, index) }
               </g>
             ))}
           </g>
